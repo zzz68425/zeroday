a3c33c2be360a8b54689693c3a44d1fe16bec682
zzz68425 <120552695+zzz68425@users.noreply.github.com>
2025-07-25 04:58:39 +0800

    Delete lib/config.py

--- lib/config.py
+++
@@ -1,5 +0,0 @@
-JINA_API_KEY="jina_9fc03fd4542e461ba6ef5cecf4d393426ZrBYDEkUL107ltbt6JHvBv7g9z6"
-
-#改成自己需要的mail
-SENDER_EMAIL="xxxxxxx@gmail.com"
-RECEIVER_EMAIL="xxxxxx@gmail.com, xxxxxx@gmail.com" #多個MAIL之間需逗號隔開
