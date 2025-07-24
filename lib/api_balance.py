a81cfa5ac1c10c52bbbed210dd9a4de191bd5a40
zzz68425 <120552695+zzz68425@users.noreply.github.com>
2025-07-25 05:00:11 +0800

    Update api_balance.py
    偷偷改的

--- lib/api_balance.py
+++ lib/api_balance.py
@@ -1,6 +1,9 @@
 #用來查餘額
 import requests
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 API_KEY = os.getenv("JINA_API_KEY")
 url = f"https://embeddings-dashboard-api.jina.ai/api/v1/api_key/fe_user?api_key={API_KEY}"
